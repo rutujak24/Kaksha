73c5e74477dce68bfe1bd664b0bccba0f7422ac9
Rutuja Kawade <56619747+rutujak24@users.noreply.github.com>
2021-11-25 22:32:14 +0530

    email update

--- core/settings.py
+++ core/settings.py
@@ -155,5 +155,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'rutuuuuj@gmail.com'
-EMAIL_HOST_PASSWORD = 'hddhhjitdxgrexn'
+EMAIL_HOST_USER = 'rutuuuujaaaa@gmail.com'
+EMAIL_HOST_PASSWORD = '' #to be added
